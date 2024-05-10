0656695ba6391c969fe26cc8bb71009e6759ef6d
Mariele Santos <marielecsantos@outlook.com>
2024-05-09 22:29:04 -0300

    Cria o projeto angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    UserComponent,
+    LoginComponent,
+    HeaderComponent,
+    ForbiddenComponent
   ],
   imports: [
     BrowserModule,

8395b5a4b1f1f9f82b0ed07a309b12d2c3307bc8
Mariele Santos <marielecsantos@outlook.com>
2024-05-20 21:03:39 -0300

    adiciona possibilidade de cadastrar produtos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './login/login.component';
 import { ForbiddenComponent } from './forbidden/forbidden.component';
 import { AuthGuard } from './_auth/auth.guard';
+import { AddNewProductComponent } from './add-new-product/add-new-product.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'admin', component: AdminComponent, canActivate:[AuthGuard], data:{roles:['Admin']} },
-  { path: 'user', component: UserComponent, canActivate:[AuthGuard], data:{roles:['User']} },
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard], data: { roles: ['Admin'] } },
+  { path: 'user', component: UserComponent, canActivate: [AuthGuard], data: { roles: ['User'] } },
   { path: 'login', component: LoginComponent },
-  { path: 'forbidden', component: ForbiddenComponent }
+  { path: 'forbidden', component: ForbiddenComponent },
+  { path: 'addNewProduct', component: AddNewProductComponent }
 ];
 
 @NgModule({
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -27,5 +27,12 @@ export class HeaderComponent implements OnInit {
     this.router.navigate(['/']);
   }
 
+  public isAdmin() {
+    return this.userAuthService.isAdmin();
+  }
+
+  public isUser() {
+    return this.userAuthService.isUser();
+  }
   
 }

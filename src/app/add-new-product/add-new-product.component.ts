3915d99a30146be86752e644ca944cca18a97d3c
Mariele Santos <marielecsantos@outlook.com>
2024-05-22 21:41:21 -0300

    adiciona campos para cadastrar imagens dos produtos

--- src/app/add-new-product/add-new-product.component.ts
+++ src/app/add-new-product/add-new-product.component.ts
@@ -3,6 +3,8 @@ import { Product } from '../_model/product.model';
 import { NgForm } from '@angular/forms';
 import { ProductService } from '../_services/product.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { FileHandle } from '../_model/file-handle.model';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-add-new-product',
@@ -15,16 +17,21 @@ export class AddNewProductComponent implements OnInit {
     productName: "",
     productDescription: "",
     productDiscountedPrice: 0,
-    productActualPrice: 0
+    productActualPrice: 0,
+    productImages: []
   }
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+    private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
   }
 
   addProduct(productForm: NgForm) {
-    this.productService.addProduct(this.product).subscribe(
+
+    const productFormData = this.prepareFormData(this.product);
+    
+    this.productService.addProduct(productFormData).subscribe(
       (response: Product) => {
         productForm.reset();
       },
@@ -34,4 +41,38 @@ export class AddNewProductComponent implements OnInit {
     );
   }
 
+  prepareFormData(product: Product): FormData {
+    const formData = new FormData();
+
+    formData.append(
+      'product',
+      new Blob([JSON.stringify(product)], {type: 'application/json'})
+    );
+
+    for (var i = 0; i < product.productImages.length; i++) {
+      formData.append(
+        'imageFile',
+        product.productImages[i].file,
+        product.productImages[i].file.name
+      );
+    }
+
+    return formData;
+  }
+
+
+  onFileSelected(event) {
+    if (event.target.files) {
+      const file = event.target.files[0];
+
+      const fileHandle: FileHandle = {
+        file: file,
+        url: this.sanitizer.bypassSecurityTrustUrl(
+          window.URL.createObjectURL(file)
+        )
+      }
+
+      this.product.productImages.push(fileHandle);
+    }
+  }
 }

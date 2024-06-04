942c9885d7515e641a3719b4a2a1e5b6ea428674
Mariele Santos <marielecsantos@outlook.com>
2024-06-03 22:25:04 -0300

    cria a home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../_services/product.service';
+import { map } from 'rxjs/operators';
+import { Product } from '../_model/product.model';
+import { ImageProcessingService } from '../image-processing.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  productDetails = [];
+
+  constructor(private productService: ProductService,
+    private imageProcessingService: ImageProcessingService
+  ) { }
 
   ngOnInit(): void {
+    this.getAllProducts();
   }
 
+  public getAllProducts() {
+    this.productService.getAllProducts()
+      .pipe(
+        map((x: Product[], i) => x.map((product: Product) => this.imageProcessingService.createImages(product)))
+      )
+      .subscribe(
+        (resp: Product[]) => {
+          console.log(resp);
+          this.productDetails = resp;
+        }, (error: HttpErrorResponse) => {
+          console.log(error);
+        }
+      );
+  }
 }

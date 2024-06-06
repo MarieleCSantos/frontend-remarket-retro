e92e52930c0bdafc2670d5d7c529beaf394b85f4
Mariele Santos <marielecsantos@outlook.com>
2024-06-05 21:08:46 -0300

    cria a página de detalhes de produto

--- src/app/product-view-details/product-view-details.component.ts
+++ src/app/product-view-details/product-view-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../_model/product.model';
 
 @Component({
   selector: 'app-product-view-details',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductViewDetailsComponent implements OnInit {
 
-  constructor() { }
+  selectedProductIndex = 0;
+  product: Product;
+
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.product = this.activatedRoute.snapshot.data['product'];
+    console.log(this.product);
+  }
+
+  changeIndex(index) {
+    this.selectedProductIndex = index;
   }
 
 }

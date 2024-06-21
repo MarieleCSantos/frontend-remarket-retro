f584ba786eede74a37003b3bc986ef4ad8b3ff60
Mariele Santos <marielecsantos@outlook.com>
2024-06-20 22:37:50 -0300

    cria pagina de registro de novo usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { ProductViewDetailsComponent } from './product-view-details/product-view
 import { BuyProductComponent } from './buy-product/buy-product.component';
 import { BuyProductResolverService } from './buy-product-resolver.service';
 import { OrderConfirmationComponent } from './order-confirmation/order-confirmation.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
@@ -73,6 +74,10 @@ const routes: Routes = [
     component: OrderConfirmationComponent,
     canActivate: [AuthGuard],
     data: { roles: ["User"] }
+  },
+  {
+    path: "register",
+    component: RegisterComponent
   }
 ];
 
